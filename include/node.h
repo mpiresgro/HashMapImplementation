09d3aad4c7b96bcad0432590ad950b96eaca6612
Mauro Pires <mpiresgro@gmail.com>
2021-10-29 23:55:22 +0100

    Added getKey to HashNode

--- include/node.h
+++ include/node.h
@@ -7,10 +7,15 @@ public:
     HashNode(K key, V value) : m_key(key), m_value(value), m_next(NULL)
     {
     }
-
-    V getValue()
+    
+    V getValue() const
     {
         return m_value;
+    }
+
+    K getKey() const
+    {
+        return m_key;
     }
 
     
